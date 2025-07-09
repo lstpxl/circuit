202acbae7a834c6ed7f59b8dc512917619709cf4
lstpxl <115099687+lstpxl@users.noreply.github.com>
2025-07-09 14:33:55 +0200

    Fix credits bg

--- src/components/AuthorCredits.tsx
+++ src/components/AuthorCredits.tsx
@@ -16,7 +16,7 @@ function GithubLogo() {
 
 export default function AuthorCredits() {
 	return (
-		<div className="fixed bottom-4 right-4 z-50 text-neutral-600 text-sm flex items-baseline gap-4">
+		<div className="fixed bottom-4 right-4 z-50 text-neutral-600 text-sm flex items-baseline gap-4 bg-background/50 rounded px-4 py-1">
 			<span>&copy; 2025 Ilia Pavlov</span>
 			<a
 				href="https://github.com/lstpxl/circuit"
