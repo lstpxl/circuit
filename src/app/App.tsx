ad9218451cad365f25a94f1275fa5e0c602a4f4b
lstpxl <115099687+lstpxl@users.noreply.github.com>
2025-08-11 18:03:40 +0200

    Clean extra logs

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,8 +4,6 @@ import { getBaseUrl } from "@/shared/config/env";
 import "./index.css";
 import { routeTree } from "./routes";
 
-console.log("App initialized with base URL:", getBaseUrl());
-
 const router = createRouter({
 	routeTree,
 	basepath: getBaseUrl(),
